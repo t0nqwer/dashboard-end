444487bad49c0b337840b6a394973b2d2165b12f
t0nqwer <oottoonnoo@gmail.com>
2023-06-27 17:17:42 +0700

    master

--- controller/website.js
+++ controller/website.js
@@ -43,6 +43,222 @@ export const GetProductforWeb = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+export const GetProduct = async (req, res) => {
+  const { search, page, query } = req.query;
+  const arr = req.body;
+  console.log(arr);
+  const limit = 30;
+  const numberStartIndex = (+page - 1) * limit;
+  try {
+    const countCloth = await prisma.product_Cloth.count({
+      where: {
+        product_id: {
+          notIn: arr,
+        },
+        code: { contains: query },
+        OR: [
+          {
+            code: { contains: search },
+          },
+          {
+            fabric: {
+              Type: {
+                name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Color: {
+                FabricColorTechnique_name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Pattern: {
+                FabricPatternName: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Weaving: {
+                weaving_name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            design: { Design_Name: { contains: search } },
+          },
+          {
+            design: {
+              Category: { Design_Category_Name: { contains: search } },
+            },
+          },
+          {
+            design: {
+              Brand: {
+                DesignBrand_Name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            design: {
+              Pattern: {
+                Pattern_Design_Name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            description: {
+              contains: search,
+            },
+          },
+        ],
+      },
+    });
+    const numberPage = countCloth / limit;
+    const ClothProduct = await prisma.product_Cloth.findMany({
+      select: {
+        product_id: true,
+        code: true,
+        fabric: {
+          select: {
+            Fabric_ID: true,
+            Weaving: true,
+            Color: true,
+            Pattern: true,
+            Type: true,
+          },
+        },
+        description: true,
+        design: true,
+        Front_Thumbnail: true,
+        price: true,
+      },
+      where: {
+        product_id: {
+          notIn: arr,
+        },
+        code: { contains: query },
+        OR: [
+          {
+            code: { contains: search },
+          },
+          {
+            fabric: {
+              Type: {
+                name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Color: {
+                FabricColorTechnique_name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Pattern: {
+                FabricPatternName: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            fabric: {
+              Weaving: {
+                weaving_name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            design: { Design_Name: { contains: search } },
+          },
+          {
+            design: {
+              Category: { Design_Category_Name: { contains: search } },
+            },
+          },
+          {
+            design: {
+              Brand: {
+                DesignBrand_Name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            design: {
+              Pattern: {
+                Pattern_Design_Name: {
+                  contains: search,
+                },
+              },
+            },
+          },
+          {
+            description: {
+              contains: search,
+            },
+          },
+        ],
+      },
+      skip: numberStartIndex,
+      take: limit,
+    });
+    const Cloth = ClothProduct.map((e) => {
+      return {
+        Id: e.product_id,
+        code: e.code,
+        price: e.price,
+        designname: e.design.Design_Name,
+        Front_Thumbnail: e.Front_Thumbnail,
+        fabric:
+          e.fabric.Color.FabricColorTechnique_ID !== 1
+            ? `ผ้า${e.fabric.Type.name}${e.fabric.Weaving.weaving_name}ย้อมสี${
+                e.fabric.Color.FabricColorTechnique_name
+              }${
+                e?.fabric?.Pattern?.FabricPatternName
+                  ? e?.fabric?.Pattern?.FabricPatternName
+                  : ""
+              }`
+            : `ผ้า${e.fabric.Type.name}${e.fabric.Weaving.weaving_name}${
+                e?.fabric?.Pattern?.FabricPatternName
+                  ? e?.fabric?.Pattern?.FabricPatternName
+                  : ""
+              }`,
+      };
+    });
+    res.status(200).json({ Cloth, page: numberPage });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+};
 export const GetSingleProductforWeb = async (req, res) => {
   const { id } = req.params;
 
--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   GetHero,
+  GetProduct,
   GetProductforWeb,
   GetSingleProductforWeb,
 } from "../controller/website.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 router.get("/products", GetProductforWeb);
 router.get("/Hero", GetHero);
 router.get("/SingleProduct/:id", GetSingleProductforWeb);
+router.post("/NewWeb", GetProduct);
 
 export default router;
